67ea55e4cd9544ff2b3dd5415c95f3a7f680fc81
Wesley Garvin <garvinwes@gmail.com>
2023-08-10 18:52:00 -0400

    Implement connection to MongoDB. - Install dotenv dependency. - Update .gitignore with .env.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+const mongo_uri = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB");
+  });
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${port}`);
