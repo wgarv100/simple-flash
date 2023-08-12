d7fe0d17c7d1de233a5286106ae4adbdd39b64c6
Wesley Garvin <garvinwes@gmail.com>
2023-08-12 18:22:29 -0400

    Implement GET request functionality.

--- backend/routes/flashcards.js
+++ backend/routes/flashcards.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const flashcardController = require("../controllers/flashcardController");
 
-// router.get("/", flashcardController.getFlashcards);
+router.get("/", flashcardController.getFlashcards);
 
 router.post("/", flashcardController.postFlashcards);
 
