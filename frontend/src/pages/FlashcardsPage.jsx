8eeabf4acbc666ee81e0307ce9dc152dad838744
Wesley Garvin <garvinwes@gmail.com>
2023-08-21 20:51:30 -0400

    Add Sidebar to FlashcardsPage.

--- frontend/src/pages/FlashcardsPage.jsx
+++ frontend/src/pages/FlashcardsPage.jsx
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getFlashcardsByGroup } from "../services/flashcardServices";
+import Grid from "@mui/material/Grid";
+import {
+  getAllGroups,
+  getFlashcardsByGroup,
+} from "../services/flashcardServices";
 import FlashcardList from "../components/FlashcardList";
+import Sidebar from "../components/Sidebar";
 
 const FlashcardsPage = () => {
   const { groupId } = useParams();
   const [flashcards, setFlashcards] = useState([]);
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    getAllGroups()
+      .then((data) => setGroups(data))
+      .catch((error) => console.error("Error fetching groups:", error));
+  }, []);
 
   useEffect(() => {
     // Fetch flashcards for the selected group
@@ -15,10 +27,14 @@ const FlashcardsPage = () => {
   }, [groupId]);
 
   return (
-    <div>
-      <h1>Flashcards in Group</h1>
-      <FlashcardList flashcards={flashcards} />
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={2}>
+        <Sidebar groups={groups} />
+      </Grid>
+      <Grid item xs={10}>
+        <FlashcardList flashcards={flashcards} groupId={groupId} />
+      </Grid>
+    </Grid>
   );
 };
 
