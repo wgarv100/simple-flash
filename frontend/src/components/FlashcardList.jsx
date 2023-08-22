89e6c6e9367e5db840b0540d59b415febda43d3a
Wesley Garvin <garvinwes@gmail.com>
2023-08-21 21:00:17 -0400

    Style FlashcardList using MUI card component.

--- frontend/src/components/FlashcardList.jsx
+++ frontend/src/components/FlashcardList.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 const FlashcardList = ({ flashcards }) => {
   return (
     <div className="flashcard-list">
-      <ul>
-        {flashcards.map((flashcard) => (
-          <li key={flashcard._id}>
-            <h3>{flashcard.title}</h3>
-            <p>{flashcard.body}</p>
-          </li>
-        ))}
-      </ul>
+      {flashcards.map((flashcard) => (
+        <Card
+          key={flashcard._id}
+          variant="outlined"
+          style={{ marginBottom: "10px" }}
+        >
+          <CardContent>
+            <Typography variant="h6" component="h3">
+              {flashcard.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              {flashcard.body}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 };
