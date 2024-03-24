c17b6626ae99496f8b305a148b273defb198f1e5
Wes Garvin <garvinwes@gmail.com>
2024-03-24 12:02:36 -0400

    Enable real-time update of flashcard list when they are added or deleted.

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -14,27 +14,36 @@ import { getAllGroups } from "../services/groupServices";
 
 // Hooks
 import { useGroupDeletion } from "../hooks/useGroupDeletion";
-import { useGetFlashcards } from "../hooks/useGetFlashcards";
+// import { useGetFlashcards } from "../hooks/useGetFlashcards";
 
 // Handlers
 import { handleOpenModal, handleCloseModal } from "../handlers/modalHandlers";
 import { handleFlashcardAdded } from "../handlers/flashcardAdditionHandlers";
 import { handleFlashcardDeletion } from "../handlers/flashcardDeletionHandlers";
 import DashboardSpeedDial from "../components/DashboardSpeedDial";
+import { getFlashcardsByGroup } from "../services/flashcardServices";
 
 const FlashcardsPage = () => {
   const { groupId } = useParams();
+
+  const [flashcards, setFlashcards] = useState([]);
   const [groups, setGroups] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { handleDeleteGroup } = useGroupDeletion();
-  const { groupName, flashcards } = useGetFlashcards();
+  // const { groupName } = useGetFlashcards();
 
   useEffect(() => {
     getAllGroups()
       .then((data) => setGroups(data))
       .catch((error) => console.error("Error fetching groups:", error));
   }, []);
+
+  useEffect(() => {
+    getFlashcardsByGroup(groupId)
+      .then((data) => setFlashcards(data))
+      .catch((error) => console.error("Error fetching flashcards:", error));
+  }, [flashcards]);
 
   return (
     <div>
