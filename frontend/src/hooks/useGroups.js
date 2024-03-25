0d71b0b508153dc8deaf5f7aa71746d895ac2a6f
Wes Garvin <garvinwes@gmail.com>
2024-03-25 19:51:08 -0400

    - Make confirm delete button switch back to delete.

--- frontend/src/hooks/useGroups.js
+++ frontend/src/hooks/useGroups.js
@@ -3,11 +3,17 @@ import { getAllGroups } from "../services/groupServices";
 
 export const useGroups = () => {
   const [groups, setGroups] = useState([]);
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
   useEffect(() => {
     getAllGroups()
       .then((data) => setGroups(data))
       .catch((error) => console.error("Error fetching groups:", error));
+  }, [groups]);
+
+  useEffect(() => {
+    // Reset confirmDelete state when groups changes
+    setConfirmDelete(false);
   }, [groups]);
 
   return groups;
